970f755c8f727ada477cb464e4995db4d3ba2db7
Jakub Mazur <j.mazur.nl@gmail.com>
2024-06-28 15:43:19 +0200

    Request Listener

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const http = require("http");
+const app = require("./app/app"); // Correct module import
 require("dotenv").config();
 
-http.createServer().listen(process.env.port, () => {
-  console.log(`Server is running on port ${process.env.port}`);
+const PORT = process.env.port || 3000; // Default to 3000 if not defined in .env
+
+http.createServer(app).listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
